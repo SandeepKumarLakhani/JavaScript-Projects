2023f48e97d1afbb61a378586539ecb413555bc8
SandeepKumarLakhani <sandeeplakhani340@gmail.com>
2024-06-23 08:23:00 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// Finally, we add logic and functionality to this app using Javascript. We do this in five easy steps:
+
+// Validate input field
+// Add a new task
+// Deleting a task
+// Crossing out a completed task
+// Clearing the input field after each entry
+
+document.querySelector('#add_task_button').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#newtask input").value = "";
+    }
+}
